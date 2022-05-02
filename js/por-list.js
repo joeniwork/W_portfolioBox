e45b07ebfa7420bf4f69d499e276245a33b4c475
joeniwork <joeniwork@gmail.com>
2022-05-02 17:38:16 +0800

    test
    123

---
+++ js/por-list.js
@@ -0,0 +1,404 @@
+var websiteLists=[
+  {
+    img: "images/porfolio/w25.jpg",
+    caseimg: "images/case/we25.png",
+    name: "DELL EMC PowerStore",
+    tab: "產品活動網站",
+    weblink: true,
+    link: "",
+    modal: "web25"
+  },
+  {
+    img: "images/porfolio/w24.jpg",
+    caseimg: "images/case/we24.png",
+    name: "CC集團 12月聖誕節慶活動",
+    tab: "遊戲活動網站",
+    weblink: true,
+    link: "https://reurl.cc/Q35rjZ",
+    modal: "web24"
+  },
+  {
+    img: "images/porfolio/w23.jpg",
+    caseimg: "images/case/we23.png",
+    name: "CC集團 11月感恩節慶活動",
+    tab: "遊戲活動網站",
+    weblink: true,
+    link: "https://reurl.cc/WLA6kD",
+    modal: "web23"
+  },
+  {
+    img: "images/porfolio/w22.jpg",
+    caseimg: "images/case/we22.png",
+    name: "CC集團 10月萬聖節慶活動",
+    tab: "遊戲活動網站",
+    weblink: true,
+    link: "https://reurl.cc/e8GR6W",
+    modal: "web22"
+  },
+  {
+    img: "images/porfolio/w21.jpg",
+    caseimg: "images/case/we21.png",
+    name: "CC集團 9月中秋節慶活動",
+    tab: "遊戲活動網站",
+    weblink: true,
+    link: "https://reurl.cc/VX0Aj5",
+    modal: "web21"
+  },
+  {
+    img: "images/porfolio/w20.jpg",
+    caseimg: "images/case/we20.png",
+    name: "翻轉音樂活動網站",
+    tab: "活動網站",
+    weblink: false,
+    link: "僅提案",
+    modal: "web20"
+  },
+  {
+    img: "images/porfolio/w19.jpg",
+    caseimg: "images/case/we19.png",
+    name: "高雄音樂製作中心",
+    tab: "公司形象網站",
+    weblink: true,
+    link: "http://soft-world-music.tw/index.html",
+    modal: "web24"
+  },
+  {
+    img: "images/porfolio/w15.jpg",
+    caseimg: "images/case/we15.png",
+    name: "櫻花語言工坊",
+    tab: "公司形象網站",
+    weblink: false,
+    link: "網站毀損",
+    modal: "web15"
+  },
+  {
+    img: "images/porfolio/w11.jpg",
+    caseimg: "images/case/we11.png",
+    name: "陳啟川紀念網站",
+    tab: "基金會形象網站",
+    weblink: false,
+    link: "僅提案",
+    modal: "web11"
+  },
+  {
+    img: "images/porfolio/w07.jpg",
+    caseimg: "images/case/we07.png",
+    name: "昱泓工業機械零件",
+    tab: "公司產品形象網站",
+    weblink: true,
+    link: "http://yuhub.com.tw/en/main/",
+    modal: "web07"
+  },
+  {
+    img: "images/porfolio/w06.jpg",
+    caseimg: "images/case/we06.png",
+    name: "皇旗內衣",
+    tab: "公司產品形象網站",
+    weblink: false,
+    link: "網站毀損",
+    modal: "web06"
+  },
+  {
+    img: "images/porfolio/w05.jpg",
+    caseimg: "images/case/we05.png",
+    name: "統全興業",
+    tab: "公司產品形象網站",
+    weblink: false,
+    link: "網站毀損",
+    modal: "web05"
+  },
+  {
+    img: "images/porfolio/w04.jpg",
+    caseimg: "images/case/we04.png",
+    name: "軒登國際腳踏車",
+    tab: "公司產品形象網站",
+    weblink: false,
+    link: "網站毀損",
+    modal: "web04"
+  },
+  {
+    img: "images/porfolio/w03.jpg",
+    caseimg: "images/case/we03.png",
+    name: "昌澤機械",
+    tab: "公司產品形象網站",
+    weblink: true,
+    link: "http://new.changtjer.com.tw/tw/main/",
+    modal: "web03"
+  },
+  {
+    img: "images/porfolio/w02.jpg",
+    caseimg: "images/case/we02.png",
+    name: "利到創新科技",
+    tab: "公司產品形象網站",
+    weblink: false,
+    link: "網站毀損",
+    modal: "web02"
+  },
+  {
+    img: "images/porfolio/w01.jpg",
+    caseimg: "images/case/we01.png",
+    name: "正益工業汽車零件",
+    tab: "公司產品形象網站",
+    weblink: false,
+    link: "網站毀損",
+    modal: "web01"
+  }
+];
+
+var designLists=[
+  {
+    img: "images/porfolio/de18.jpg",
+    caseimg: "images/case/de18.png",
+    name: "(提案) 遊戲活動 Kv 設計",
+    tab: "Kv 設計",
+    modal: "de18",
+    modal_size: "modal-lg"
+  },
+  {
+    img: "images/porfolio/de19.jpg",
+    caseimg: "images/case/de19.png",
+    name: "遊戲活動 Kv 設計",
+    tab: "Kv 設計",
+    modal: "de18",
+    modal_size: "modal-lg"
+  },
+  {
+    img: "images/porfolio/de20.jpg",
+    caseimg: "images/case/de20.png",
+    name: "遊戲活動 Kv 設計",
+    tab: "Kv 設計",
+    modal: "de20",
+    modal_size: "modal-lg"
+  },
+  {
+    img: "images/porfolio/de21.jpg",
+    caseimg: "images/case/de21.png",
+    name: "科技會議 Kv 設計",
+    tab: "Kv 設計",
+    modal: "de21",
+    modal_size: "modal-lg"
+  },
+  {
+    img: "images/porfolio/de01.jpg",
+    caseimg: "images/case/de01.png",
+    name: "公司員工活動",
+    tab: "公司文宣",
+    modal: "de01",
+    modal_size: "modal-lg"
+  },
+  {
+    img: "images/porfolio/de02.jpg",
+    caseimg: "images/case/de02.png",
+    name: "公司聖誕節慶畫面",
+    tab: "節慶視覺設計",
+    modal: "de02",
+    modal_size: "modal-lg"
+  },
+  {
+    img: "images/porfolio/de03.jpg",
+    caseimg: "images/case/de03.png",
+    name: "公司端午節慶畫面",
+    tab: "節慶視覺設計",
+    modal: "de03",
+    modal_size: "modal-lg"
+  },
+  {
+    img: "images/porfolio/de04.jpg",
+    caseimg: "images/case/de04.png",
+    name: "會議規範",
+    tab: "公司文宣設計",
+    modal: "de04",
+    modal_size: "modal-lg"
+  },
+  {
+    img: "images/porfolio/de05.jpg",
+    caseimg: "images/case/de05.png",
+    name: "無限創意凝聚活動",
+    tab:  "公司活動文宣設計",
+    modal: "de05",
+    modal_size: "modal-sm"
+  },
+  {
+    img: "images/porfolio/de06.jpg",
+    caseimg: "images/case/de06.png",
+    name: "ColorfulParty 尾牙拍貼機",
+    tab:  "公司文宣設計",
+    modal: "de06",
+    modal_size: "modal-lg"
+  },
+  {
+    img: "images/porfolio/de07.jpg",
+    caseimg: "images/case/de07.png",
+    name: "(提案) 雙11節慶登入頁",
+    tab:  "Kv設計",
+    modal: "de07",
+    modal_size: "modal-lg"
+  },
+  {
+    img: "images/porfolio/de08.jpg",
+    caseimg: "images/case/de08.png",
+    name: "遊戲活動 Banner",
+    tab:  "Banner 設計",
+    modal: "de08",
+    modal_size: "modal-lg"
+  },
+  {
+    img: "images/porfolio/de10.jpg",
+    caseimg: "images/case/de10.png",
+    name: "錄音室-宣傳 DM",
+    tab:  "公司文宣設計",
+    modal: "de09",
+    modal_size: "modal-sm"
+  },
+  {
+    img: "images/porfolio/de11.jpg",
+    caseimg: "images/case/de11.png",
+    name: "遊戲 Youtube 封面",
+    tab:  "Youtube 封面",
+    modal: "de11",
+    modal_size: "modal-sm"
+  },
+  {
+    img: "images/porfolio/de12.jpg",
+    caseimg: "images/case/de12.png",
+    name: "手遊節慶網路宣傳",
+    tab:  "Banner 設計",
+    modal: "de12",
+    modal_size: "modal-lg"
+  },
+  {
+    img: "images/porfolio/de13.jpg",
+    caseimg: "images/case/de13.png",
+    name: "(提案)公司部門 icon 設計",
+    tab:  "公司文宣設計",
+    modal: "de13",
+    modal_size: "modal-lg"
+  },
+  {
+    img: "images/porfolio/de14.jpg",
+    caseimg: "images/case/de14.png",
+    name: "(提案)公司部門 LOGO 設計",
+    tab:  "LOGO 設計",
+    modal: "de14",
+    modal_size: "modal-sm"
+  },
+  {
+    img: "images/porfolio/de16.jpg",
+    caseimg: "images/case/de16.png",
+    name: "遊戲公司立旗設計",
+    tab:  "公司文宣設計",
+    modal: "de16",
+    modal_size: "modal-sm"
+  }
+];
+
+var skill_txList=[
+  {
+    icon: "images/sublime.svg",
+    name: "Subline"
+  },
+  {
+    icon: "images/brackets.svg",
+    name: "Brackets"
+  },
+  {
+    icon: "images/vs_code.svg",
+    name: "VS Code"
+  }
+];
+
+var skill_dsList=[
+  {
+    icon: "images/adobe-ps.svg",
+    name: "Photoshop"
+  },
+  {
+    icon: "images/adobe-ai.svg",
+    name: "Illustrator"
+  },
+  {
+    icon: "images/adobe-xd.svg",
+    name: "Adobe XD"
+  }
+];
+
+var skill_websList=[
+  {
+    icon: "images/html5.svg",
+    name: "Html & css"
+  },
+  {
+    icon: "images/sass.svg",
+    name: "Sass"
+  },
+  {
+    icon: "images/bootstrap.svg",
+    name: "Bootstrap"
+  }
+];
+
+var skill_gitsList=[
+  {
+    icon: "images/tortoisesvn.png",
+    name: "tortoise svn"
+  },
+  {
+    icon: "images/sourcetree.svg",
+    name: "Sourcetree"
+  },
+  {
+    icon: "images/gitlab.svg",
+    name: "Git Lab"
+  }
+];
+
+var compexList=[
+  {
+    y_begin: "2012",
+    y_end: "2012",
+    name: "偉冠科技"
+  },
+  {
+    y_begin: "2012",
+    y_end: "2014",
+    name: "石頭資訊"
+  },
+  {
+    y_begin: "2014",
+    y_end: "2015",
+    name: "傑瑀企業"
+  },
+  {
+    y_begin: "2015",
+    y_end: "2015",
+    name: "朋盟國際"
+  },
+  {
+    y_begin: "2015",
+    y_end: "2017",
+    name: "頤瑞科技"
+  },
+  {
+    y_begin: "2017",
+    y_end: "2019",
+    name: "智冠科技"
+  },
+  {
+    y_begin: "2019",
+    y_end: "NOW",
+    name: "無限創意"
+  }
+];
+
+var vm = new Vue({
+  el: '#container',
+  data:{
+    websites: websiteLists,
+    designs: designLists,
+    skill_ts: skill_txList,
+    skill_ds: skill_dsList,
+    skill_webs: skill_websList,
+    skill_gits: skill_gitsList,
+    compexs: compexList
+  }
+});
